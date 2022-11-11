cfa0d3095d11816e32f1ae8630a886f45a10a61c
Simon Beaulieu <sim.beaulieu10@gmail.com>
2022-11-11 15:18:05 -0500

    etape template produit

--- routes/html.js
+++ routes/html.js
@@ -21,4 +21,8 @@ router.get('/points_de_vente', function(req, res){
     res.sendFile(path.join(__dirname + '/../client/points_de_vente.html'));
 });
 
+router.get('/produit', function(req, res){
+    res.sendFile(path.join(__dirname + '/../client/produit.html'));
+});
+
 module.exports = router
